4e2851ef681c4a9e6b7bd3e1632afc58201adbc9
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-08 16:31:11 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -6,7 +6,7 @@ def bonus
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercutio", age: "18", attitude: "confused"}
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -14,7 +14,7 @@ def bonus
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Steven", age: "30", attitude: "hot-headed"}, 
+          {name: "Steven", age: "30", attitude: "confused"}, 
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
